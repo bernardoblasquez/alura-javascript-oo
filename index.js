8bf4658caab3f7602c8ba61aff09136e7ee6fef5
Bernardo (John.bt) <bernardo.bt@gmail.com>
2020-08-25 09:13:52 -0300

    criação da classe ContaCorrente, seus métodos e uso do conceito de early return

--- index.js
+++ index.js
@@ -2,16 +2,56 @@
 class Cliente {
     nome;
     cpf;
+   // conta;
+}
+
+class ContaCorrente{
     agencia;
     saldo;
-    rg;
+
+    sacar(valor){
+        if (this.saldo >= valor){
+            this.saldo -= valor;
+            return valor;
+        } 
+    }
+
+    depositar(valor){
+        if (valor <= 0) return // early return;
+        this.saldo += valor;
+
+        // retornar o valor de uma função ou método o quanto antes
+        // - aumenta: legibilidade e performance
+        // - facilita a manutenção 
+        // - previne bugs
+
+        // a melhoria da legibilidade ocorre porque passamos a evitar 
+        // if aninhados que pioram muita a legibilidade do código
+        
+        // a performance melhora pois deixamos de perforrer uma série de ifs e elses ,
+        // já que estamos retornando o mais rápido possível.
+    }
 }
 
 const cliente01 = new Cliente();
-cliente01.nome = "Be";
+cliente01.nome = "Ricardo";
 cliente01.cpf = "13343545345";
-cliente01.agencia = "001";
-cliente01.saldo = 0;
-cliente01.rg = 332343423;
+
+const cliente02 = new Cliente();
+cliente02.nome = "Alice";
+cliente02.cpf = "123344221"
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo);
+contaCorrenteRicardo.sacar(50);
+console.log(contaCorrenteRicardo);
+contaCorrenteRicardo.depositar(-1);
+console.log(contaCorrenteRicardo);
+contaCorrenteRicardo.depositar(500);
+console.log(contaCorrenteRicardo);
+
 
 console.log(cliente01);
+console.log(cliente02)
