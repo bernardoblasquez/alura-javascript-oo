b5fea43beaf2240f1cfa9eea35725469456c7f2d
Bernardo (John.bt) <bernardo.bt@gmail.com>
2020-08-25 08:28:31 -0300

    Criação da classe Cliente

---
+++ index.js
@@ -0,0 +1,17 @@
+
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+    rg;
+}
+
+const cliente01 = new Cliente();
+cliente01.nome = "Be";
+cliente01.cpf = "13343545345";
+cliente01.agencia = "001";
+cliente01.saldo = 0;
+cliente01.rg = 332343423;
+
+console.log(cliente01);
