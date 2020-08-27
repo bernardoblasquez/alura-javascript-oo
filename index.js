f57a1e4dba2b6c3df25bec35098b4e025f355a47
Bernardo (John.bt) <bernardo.bt@gmail.com>
2020-08-27 08:40:05 -0300

    Atualização do index para testar as classes desenvolvidas

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente01 = new Cliente("Ricardo", "13343545345");
 const contaCorrenteRicardo = new ContaCorrente(1001, cliente01);
-contaCorrenteRicardo.saldo = 100;
+contaCorrenteRicardo.depositar(100) ;
+
 
 
 const cliente02 = new Cliente("Alice", "123344221");
 const contaCorrenteAlice = new ContaCorrente(102, cliente02);
-contaCorrenteAlice.saldo = 500;
+contaCorrenteAlice.depositar(500);
 
 
 console.log(contaCorrenteRicardo);
@@ -19,4 +20,4 @@ console.log("------------------ transferencia ---------------------")
 
 contaCorrenteAlice.transferir(50, contaCorrenteRicardo);
 console.log(contaCorrenteRicardo);
-console.log(contaCorrenteAlice);
+console.log(contaCorrenteAlice, ContaCorrente.numeroDeContas);
