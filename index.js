79543c3a9b8f47fdcf8591b934326596c3c6a9b3
Bernardo (John.bt) <bernardo.bt@gmail.com>
2020-08-30 10:31:26 -0300

    Testes das classes refatoradas e sobrescritas.

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ import { ContaPoupanca } from "./ContaPoupanca.js";
 const cliente01 = new Cliente("Ricardo", "13343545345");
 const contaCorrenteRicardo = new ContaCorrente(1001, cliente01);
 contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100)
+contaCorrenteRicardo.sacar(100);
+
+console.log(contaCorrenteRicardo);
 
 const contaPoupanca = new ContaPoupanca(50, cliente01, 1001);
-
-
+contaPoupanca.depositar(400);
+contaPoupanca.sacar(100);
+contaPoupanca.teste();
 
 console.log(contaPoupanca);
-console.log(contaCorrenteRicardo);
+
 
