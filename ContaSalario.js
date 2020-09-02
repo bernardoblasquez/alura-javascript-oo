f5fbfee44ed92f4c429ea134e476fcb7d55511bc
Bernardo (John.bt) <bernardo.bt@gmail.com>
2020-09-02 18:16:54 -0300

    arquivos movidos para pasta conta

--- ContaSalario.js
+++
@@ -1,9 +0,0 @@
-import {Conta} from "./Conta.js";
-
-export class ContaSalario extends Conta{ 
-    
-    constructor (sladoInicial, Cliente, Agencia){
-        super (sladoInicial, Cliente, Agencia)
-    }
-    
-}
