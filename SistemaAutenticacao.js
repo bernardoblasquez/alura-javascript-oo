a5021b3460729a4645284e96ad26ad56cafbc4cf
Bernardo (John.bt) <bernardo.bt@gmail.com>
2020-09-03 09:12:24 -0300

    Mudança na implementação da classe. A  responsabilidade de autenticar agora é dos objetos passados para a classe SistemaAutenticacao.

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -1,17 +1,34 @@
 /*
-    Ser autenticável significa ter a propriedade Senha
+    Ser autenticável significa ter a o método senha
 */
 
 export class  SistemaAutenticacao{
     
-    static login (funcionario, senha){
-        console.log("valor de funcionario:", funcionario);
-        console.log("valor do metodo funcionario.senha: " + funcionario.senha);
-        return funcionario.senha == senha;
+    static login (autenticavel, senha){
+        return autenticavel.autenticar(senha);
     }
-
-    // nessa implementação, ao passar a classe cliente como atributo (funcionário)
-    // e a senha igual ao definida em cliente o método login retornará FALSO 
-    // ao invés de um erro, pois o atributo senha será criado em tempo de execução
-    // 
 }
+
+// a mudança da implementação passa para o objeto passado pelo atributo
+// 'atenticável' a responsabilidade de ser eutenticável. Dessa forma tanto 
+// Cliente quanto funcionário e seus fihos serão autenticáveis somente se
+// possuirem o atributo "autentica". Caso não possuam um erro é retornado
+
+
+/*
+    --- Na implementação abaixo ser autenticável significa ter a PROPRIEDADE 'senha'
+
+    export class  SistemaAutenticacao{
+        
+        static login (funcionario, senha){
+            console.log("valor de funcionario:", funcionario);
+            console.log("valor do metodo funcionario.senha: " + funcionario.senha);
+            return funcionario.senha == senha;
+        }
+
+        // nessa implementação, ao passar a classe cliente como atributo (funcionário)
+        // e a senha igual ao definida em cliente o método login retornará FALSO 
+        // ao invés de um erro, pois o atributo senha será criado em tempo de execução
+        // 
+    }
+*/
